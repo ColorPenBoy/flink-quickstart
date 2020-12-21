9a411050faaabb3206470955ef1cf2e8c8e04349
colorpen <colorpenboy@163.com>
2020-12-21 13:55:15 +0800

    init proj

---
+++ src/main/java/com/eryanet/SocketWindowWordCount.java
@@ -0,0 +1,96 @@
+package com.eryanet;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.common.functions.ReduceFunction;
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.util.Collector;
+
+/**
+ * @BelongsProject: flink-quickstart-java
+ * @BelongsPackage: PACKAGE_NAME
+ * @Author: zhangqiang
+ * @CreateTime: 2020-07-09 14:58
+ * @Description:
+ */
+public class SocketWindowWordCount {
+
+    public static void main(String[] args) throws Exception {
+
+        // 获取域名及端口号
+        final String hostname;
+        final int port;
+        try {
+            final ParameterTool params = ParameterTool.fromArgs(args);
+            hostname = params.has("hostname") ? params.get("hostname") : "localhost";
+            port = params.getInt("port");
+        } catch (Exception e) {
+            System.err.println("No port specified. Please run 'com.eryanet.SocketWindowWordCount " +
+                    "--hostname <hostname> --port <port>', where hostname (localhost by default) " +
+                    "and port is the address of the text server");
+            System.err.println("To start a simple text server, run 'netcat -l <port>' and " +
+                    "type the input text into the command line");
+            return;
+        }
+
+        // 获取Flink的运行环境
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        // 连接Socket获取输入数据
+        DataStream<String> text = env.socketTextStream(hostname, port, "\n");
+
+        // parse the data, group it, window it, and aggregate the counts
+        // 统计一句话中，所有单词包含的个数，一样的单词叠加
+        DataStream<WordWithCount> windowCounts = text
+                .flatMap(new FlatMapFunction<String, WordWithCount>() {
+                    @Override
+                    public void flatMap(String value, Collector<WordWithCount> out) {
+                        String[] splits = value.split("\\s");
+                        for (String word : splits) {
+                            out.collect(new WordWithCount(word, 1L));
+                        }
+                    }
+                })
+                .keyBy("word")
+                // 时间窗口大小为2s，时间间隔为1s
+                .timeWindow(Time.seconds(5))
+                // 这里使用sum和reduce都可以
+                //.sum("count")
+                .reduce(new ReduceFunction<WordWithCount>() {
+                    @Override
+                    public WordWithCount reduce(WordWithCount a, WordWithCount b) {
+                        return new WordWithCount(a.word, a.count + b.count);
+                    }
+                });
+
+        // 把数据打印到控制台，并且设置并行度（理解为：并发线程数）
+        windowCounts.print().setParallelism(1);
+
+        // 这行代码一定要实现，否则程序不执行
+        env.execute("Socket Window WordCount");
+    }
+
+    /**
+     * Data type for words with count.
+     */
+    public static class WordWithCount {
+
+        public String word;
+        public long count;
+
+        public WordWithCount() {}
+
+        public WordWithCount(String word, long count) {
+            this.word = word;
+            this.count = count;
+        }
+
+        @Override
+        public String toString() {
+            return word + " : " + count;
+        }
+    }
+
+}
